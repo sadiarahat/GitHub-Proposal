1921649dbaa565ac1e8d21f2fddde2e5d9659b50
sadiarahat <117492117+sadiarahat@users.noreply.github.com>
2022-11-04 20:58:28 -0400

    Add files via upload

---
+++ GitHub Proposal.py
@@ -0,0 +1,17 @@
+#Preposal
+
+##What ideas do you have for your final projects?
+- Creating a recipe book with a dictionary of sorts. If you have stuff in your pantry, but don't know what to make, we will give you recipes based on which meal you will cook
+- Create a medical Mario game
+-Create a program in which if you entered what your symptoms were by answering yes/no questions or simply listing,
+we would tell you the potential disorder/problem that you are facing
+-program/app of sorts that would guide you through solutions to your problems/people you could talk to
+
+
+##If you plan to collaborate with one or two classmates, what are their names?
+Tameema Mamun: tuq08501@temple.edu
+Maryam Amr: tuq19794@temple.edu
+
+##Do you have any questions of your own?
+Is it possible to work in a group of 4?
+How intricate should the project be?
