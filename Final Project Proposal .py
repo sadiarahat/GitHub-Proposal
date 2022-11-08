29cd7162f30440074804d5a26390ee27dc50d18c
sadiarahat <117492117+sadiarahat@users.noreply.github.com>
2022-11-08 18:50:53 -0500

    Add files via upload

---
+++ Final Project Proposal .py
@@ -0,0 +1,49 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+Medical Mario
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+A game about Mario but in a medical setting. Instead of trying to save a princess he's trying to retrieve the stolen vaccine.
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+Our software will be a platformer with a character that the player can control. It will have a lot of the features a conventional Mario game would have, but with original ideas in it, such as medicines (pills, cough syrup, etc) being powerups. Much of the platforms that you jump on will also be medicine related instead, such as a medicine bottle. It'll be executed by programming the game in the engine unity using c#. We'll make the art and sprites on Clip Studio Paint. We might make our own soundtrack using Ableton.
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+TODO, if applicable
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+Tameema Mamun
+tuq08501@temple.edu
+Bin Li 
+
+Sadia Rahat
+tup85173@temple.edu
+Zhengkang Fan
+
+Maryam Amr
+tuq19794@temple.edu
+Bin Li
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+A good outcome for our final project would be for the Mario game in a medical setting to be a success. This means properly designing all the maps, executing the animations, and making a Mario inspired soundtrack.
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+A better outcome for the final project would be to implement some sort of small story into the game to make it engaging.
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+The best outcome would be for the game to have a full fledged plot. 
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+We will need to learn how to develop with pygame. We'll need to research how to execute animations using python, and learning how to remix Mario MIDI files to make it sound more "medical." We're still thinking of who will be able to do what, but roughly, we're thinking of first establishing who's best at what. This is new for all of us so we'll find that out as we go. All three of us will first watch several videos on game development in pygame. Tameema might develop rough sketches/ideas for how all the settings will look, Maryam will polish the ideas and digitize them, and Sadia might execute the code.
